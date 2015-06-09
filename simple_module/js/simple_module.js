bbee3980ab37f367a86cd4c52cbd196e10dbb944
barnettech <barnettech@gmail.com>
2015-06-09 17:09:19 -0400

    adding jquery accordion to the example module.

--- simple_module/js/simple_module.js
+++ simple_module/js/simple_module.js
@@ -1,6 +1,7 @@
 (function($) {
     $(document).ready(function() {
       console.log('hello');
-      $('.title').draggable();     
+      $('#page-title').draggable();     
     });
+    $( "#accordion" ).accordion();
 }(jQuery));
